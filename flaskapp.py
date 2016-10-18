17e8d5179e35b2f20498f93d0c350037ad96bc18
Shannnnn <sacanoy16@gmail.com>
2016-10-18 23:09:43 +0800

    My Flask said Hello to You.

--- flaskapp.py
+++ flaskapp.py
@@ -1,3 +1,10 @@
 @route('/hello')
 def hello():
     return 'Hello Flask!'
+	
+greet = Flask(__name__)
+    
+@greet.route('/')
+@greet.route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}.', name=name)
